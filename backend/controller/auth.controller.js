3b830d5de5415e8505c25e778b1ff4a778cc9886
yash rai <yash2154rai@gmail.com>
2024-04-10 18:02:32 +0530

    add logout feature

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -68,5 +68,10 @@ export const login = async (req, res) => {
   }
 };
 export const logout = (req, res) => {
-  console.log("logout");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "internal server error" });
+  }
 };
