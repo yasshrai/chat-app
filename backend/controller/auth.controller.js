80b558a6181a8d5f72df4730741090b0263aad0e
yash rai <yash2154rai@gmail.com>
2024-04-10 14:15:35 +0530

    add signup feature and hashing password

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,7 +1,43 @@
+import User from "../models/user.model.js";
+import bcrypyt from "bcryptjs";
+
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmpassword, gender } = req.body;
-  } catch (error) {}
+    if (password !== confirmpassword) {
+      res.status(400).json({ error: "password do not match" });
+    }
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ error: "username alreay exists" });
+    }
+
+    const salt = await bcrypyt.genSalt(10);
+    const hashedPassword = await bcrypyt.hash(password, salt);
+
+    const boyprofilepic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlprofilepic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyprofilepic : girlprofilepic,
+    });
+    await newUser.save();
+    res.status(201).json({
+      _id: newUser.id,
+      fullName: newUser.fullName,
+      userName: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("error in signup controller", error.message);
+    res.status(500).json({
+      error: "internal server error",
+    });
+  }
 };
 export const login = (req, res) => {
   console.log("login");
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./router/auth.router.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
+dotenv.config();
+app.use(express.json());
 app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("chat app api");
+});
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
