a7dd6f3abe9e40f10a1849df3144fcf56fb407d7
yash rai <yash2154rai@gmail.com>
2024-04-10 11:25:11 +0530

    add required packages and create express server ,routes and controller

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./router/auth.router.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
