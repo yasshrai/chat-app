532c353a79f3d6c5bd1530a033681140193bef8f
yash rai <yash2154rai@gmail.com>
2024-04-11 14:56:40 +0530

    update message.model and message.controller

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -1,8 +1,32 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-    const { id } = req.params;
+    const { id: receiverId } = req.params;
     const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.message.push(newMessage._id);
+    }
+    res.status(201).json({ newMessage });
   } catch (error) {
     console.log("error in sendmessage controller:", error.message);
     res.status(500).json({ error: "internal server error" });
--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -20,4 +20,5 @@ const messageSchema = new mongoose.Schema(
   { timestamps: true }
 );
 const Message = mongoose.model("Message", messageSchema);
+
 export default Message;
