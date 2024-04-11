c3c9f769f110ad195af74d0e44836dfebf5e9251
yash rai <yash2154rai@gmail.com>
2024-04-11 17:10:37 +0530

    add get message function

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -1,6 +1,5 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
@@ -30,6 +29,25 @@ export const sendMessage = async (req, res) => {
     res.status(201).json({ newMessage });
   } catch (error) {
     console.log("error in sendmessage controller:", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userTochatId } = req.params;
+    const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userTochatId] },
+    }).populate("message");
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+    res.status(200).json(conversation.message);
+  } catch (error) {
+    console.log(error.message);
+
     res.status(500).json({ error: error.message });
   }
 };
