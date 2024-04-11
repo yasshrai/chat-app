238aa60d4a153fcad9da6d2a2588819d1cbcce03
yash rai <yash2154rai@gmail.com>
2024-04-11 19:04:53 +0530

    update user routes

--- backend/router/user.router.js
+++ backend/router/user.router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
+import { getUsersForSidebar } from "../controller/user.controller.js";
 
 const router = express.Router();
 
