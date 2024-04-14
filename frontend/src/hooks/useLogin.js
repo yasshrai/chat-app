d83ddeefbbcf3d90b3fe56962224a66d667892b1
yash rai <yash2154rai@gmail.com>
2024-04-14 19:04:46 +0530

    add data validation feature in login

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -2,11 +2,26 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { useAuthContext } from "../context/AuthContext";
 
+function handleInputError({ username, password }) {
+  if (!username || !password) {
+    toast.error("please fill all field");
+    return false;
+  }
+  return true;
+}
+
 const useLogin = () => {
   const [loading, setLoading] = useState(false);
   const { setAuthUser } = useAuthContext();
 
   const login = async (username, password) => {
+    const success = handleInputError({
+      username,
+      password,
+    });
+    if (!success) {
+      return;
+    }
     setLoading(true); // Set loading state before the async operation starts
     try {
       const res = await fetch("http://localhost:5000/api/auth/login", {
