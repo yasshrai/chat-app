7127ee080b7ca119be89d8bfbfee21ec38ead671
yash rai <yash2154rai@gmail.com>
2024-04-20 10:59:37 +0530

    format file

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3000/", {
+      const socket = io("http://localhost:5000/", {
         query: {
           userId: authUser._id,
         },
--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -3,30 +3,29 @@ import { useAuthContext } from "../context/AuthContext";
 import toast from "react-hot-toast";
 
 const useLogout = () => {
-	const [loading, setLoading] = useState(false);
-	const { setAuthUser } = useAuthContext();
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
-	const logout = async () => {
-		setLoading(true);
-		try {
-			const res = await fetch("/api/auth/logout", {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-			});
-			const data = await res.json();
-			if (data.error) {
-				throw new Error(data.error);
-			}
+  const logout = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
 
-			localStorage.removeItem("chat-user");
-			setAuthUser(null);
-		} catch (error) {
-			toast.error(error.message);
-		} finally {
-			setLoading(false);
-		}
-	};
-
-	return { loading, logout };
+      localStorage.removeItem("chat-user");
+      setAuthUser(null);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, logout };
 };
 export default useLogout;
