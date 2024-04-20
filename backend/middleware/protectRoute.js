ccca6bdf25208be8469e93f4fee4b90e10ca206d
yash rai <yash2154rai@gmail.com>
2024-04-20 16:01:00 +0530

    fix bugs

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -2,32 +2,34 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
-	try {
-		const token = req.cookies.jwt;
+  try {
+    const token = req.cookies.jwt;
 
-		if (!token) {
-			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
-		}
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized - No Token Provided" });
+    }
 
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-		if (!decoded) {
-			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
-		}
+    if (!decoded) {
+      return res.status(401).json({ error: "Unauthorized - Invalid Token" });
+    }
 
-		const user = await User.findById(decoded.userId).select("-password");
+    const user = await User.findById(decoded.userId).select("-password");
 
-		if (!user) {
-			return res.status(404).json({ error: "User not found" });
-		}
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
-		req.user = user;
+    req.user = user;
 
-		next();
-	} catch (error) {
-		console.log("Error in protectRoute middleware: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
-	}
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export default protectRoute;
--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-	{
-		senderId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-		receiverId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-		message: {
-			type: String,
-			required: true,
-		},
-		// createdAt, updatedAt
-	},
-	{ timestamps: true }
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    // createdAt, updatedAt
+  },
+  { timestamps: true }
 );
 
 const Message = mongoose.model("Message", messageSchema);
