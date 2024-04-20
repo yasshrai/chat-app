30ae300beedfd9f40cd42182124853f2726bce97
yash rai <yash2154rai@gmail.com>
2024-04-20 15:53:52 +0530

    fix bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
