ff6b919139c27a5e4c3895a983075cec005b8e12
yash2154 <97081184+yash2154@users.noreply.github.com>
2024-05-06 22:40:50 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://chatapp-project-57sf.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
